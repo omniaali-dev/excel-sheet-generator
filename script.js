c049a8257e34b64e63e7d95b59f789728bd1ce05
omnia <131707114+omniaali-dev@users.noreply.github.com>
2023-05-31 21:09:14 +0300

    Added sweetalert library CDN

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+  rows = document.getElementsByClassName("rows")[0],
+  columns = document.getElementsByClassName("columns")[0];
+tableExists = false;
+
+function checkFields() {
+  if (rows.value == "" || columns.value == "") {
+    swal("Opps!", "Please enter the number of rows and columns!", "warning");
+  } else {
+    generateTable();
+  }
+}
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
+  let rowsNumber = parseInt(rows.value),
+    columnsNumber = parseInt(columns.value);
+  table.innerHTML = "";
+  for (let i = 0; i < rowsNumber; i++) {
+    var tableRow = "";
+    for (let j = 0; j < columnsNumber; j++) {
+      tableRow += `<td contenteditable></td>`;
     }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
-    }
-}
+    table.innerHTML += tableRow;
+  }
+  if (rowsNumber > 0 && columnsNumber > 0) {
+    tableExists = true;
+  }
+};
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+  if (!tableExists) {
+    swal("Alert!", "There is no generated table to be exported!", "error");
+  } else {
+    var elt = table;
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl
+      ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+      : XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+  }
+};
